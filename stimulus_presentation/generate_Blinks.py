57ebb2db5fcc1337181b6bc00ad83f63936b5c42
matiya <matias.siracusa@gmx.com>
2017-11-01 19:58:56 -0300

    Se etiqueta parpadeo y no parpadeo también

--- stimulus_presentation/generate_Blinks.py
+++ stimulus_presentation/generate_Blinks.py
@@ -18,11 +18,14 @@ from pylsl import StreamInfo, StreamOutlet, local_clock
 #prefs.general['audioDriver'] = [u'jack']
 prefs.general['audioLib'] = ['pygame']
 
-
 parser = OptionParser()
-parser.add_option("-d", "--duration",
-                  dest="duration", type='int', default=30,
-                  help="duration of the recording in seconds.")
+parser.add_option(
+    "-d",
+    "--duration",
+    dest="duration",
+    type='int',
+    default=30,
+    help="duration of the recording in seconds.")
 
 (options, args) = parser.parse_args()
 
@@ -30,7 +33,7 @@ parser.add_option("-d", "--duration",
 info = StreamInfo('Markers', 'Markers', 1, 0, 'int32', 'myuidw43536')
 outlet = StreamOutlet(info)
 
-markernames = [0, 1]
+markernames = [1, 2]
 start = time()
 
 # Set up trial parameters
@@ -40,31 +43,39 @@ soa = 0.2
 jitter = 0.2
 record_duration = np.float32(options.duration)
 
-
 # Setup graphics
-mywin = visual.Window([900, 800], monitor='testMonitor', units='deg',
-                      fullscr=False)
+mywin = visual.Window(
+    [900, 800], monitor='testMonitor', units='deg', fullscr=False)
 # Initialize stimuli
 aud1 = sound.Sound('C', octave=5, sampleRate=44100, secs=0.3, bits=8)
 aud1.setVolume(0.8)
 path_to_stimulus = '/home/default/Workspace/pfinal//stimulus_presentation/stim/timer.webm'
-mov = visual.MovieStim3(mywin,path_to_stimulus, size=(800, 600), volume=1.0, noAudio=True)
+mov = visual.MovieStim3(
+    mywin, path_to_stimulus, size=(800, 600), volume=1.0, noAudio=True)
 start = time()
+timestamp = time()
 
 while mov.status != visual.FINISHED:
     mov.draw()
     mywin.flip()
-        # Send marker
-    
-    if((round(time() - start,2)%3)<=0.015 and (time() - start)>2):
+
+    # Send blink marker every 3 seconds
+    if ((round(time() - start, 2) % 3) <= 0.015
+            and (abs(local_clock() - timestamp)) > 1):
         timestamp = local_clock()
-        print(time() - start)
+        print("Blink", time() - start)
         aud1.play()
         outlet.push_sample([markernames[1]], timestamp)
+    else:
+        if ((round(time() - start, 2) % 2) <= 0.015
+                and (abs(local_clock() - timestamp)) > 1):
+            timestamp = local_clock()
+            print("Non blink: ", time() - start)
+            outlet.push_sample([markernames[0]], timestamp)
     if len(event.getKeys()) > 0 or (time() - start) > record_duration:
         break
 
 event.clearEvents()
-   
+
 mywin.close()
 core.quit()

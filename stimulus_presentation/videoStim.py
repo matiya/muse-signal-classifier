29b3c7dbbec9b376850ccb0b7b825dfacc21caa3
matiya <matias.siracusa@gmx.com>
2017-11-30 20:47:57 -0300

    Programa para input del usuario 3

--- stimulus_presentation/videoStim.py
+++ stimulus_presentation/videoStim.py
@@ -5,30 +5,30 @@ from random import choice
 
 import numpy as np
 from pandas import DataFrame
-from psychopy import visual, core, event, sound, prefs, gui, logging
+from psychopy import visual, core, event, sound, prefs, logging
 from pylsl import StreamInfo, StreamOutlet, local_clock
 
 prefs.general['audioLib'] = ['pygame']
 
 parser = OptionParser()
 # nombre para el logfile
-fname = ("stimulus_%s.log" % strftime("%Y-%m-%d-%H.%M.%S", gmtime()))
-logfile = logging.LogFile(fname)
+fname = ("../logs/stimulus_%s.log" % strftime("%Y-%m-%d-%H.%M.%S", gmtime()))
+logfile = logging.LogFile(fname, filemode='w')
 
 parser.add_option(
-    "-d",
-    "--duration",
-    dest="duration",
+    "-e",
+    "--experimentos",
+    dest="n_trials",
     type='int',
-    default=10,
-    help="Duracion del experimento.")
+    default=3,
+    help="Numero de experimentos")
 parser.add_option(
     "-i",
     "--interval",
     dest="iti",
     type='int',
     default=7,
-    help="Intervalo entre las pruebas.")
+    help="Intervalo entre las pruebas")
 
 (options, args) = parser.parse_args()
 
@@ -40,6 +40,9 @@ outlet = StreamOutlet(info)
 # 3 : Estimulo neutro
 markernames = [1, 2, 3]
 
+from psychopy import gui
+# Feedback del usuario
+feedback = []
 # Sonido para debug
 aud1 = sound.Sound('C', octave=5, sampleRate=44100, secs=0.3, bits=8)
 aud1.setVolume(0.8)
@@ -48,11 +51,11 @@ start = time()
 timestamp = time()
 
 # Set up trial parameters
-n_trials = 3
+n_trials = np.float32(options.n_trials)
 iti = np.float32(options.iti)
 soa = 0.2
 jitter = 0.2
-record_duration = np.float32(options.duration)
+record_duration = 1000
 # Setup trial list
 # cambiar para mostrar videos feos
 movie_type = np.random.binomial(1, 0.5, n_trials)
@@ -86,20 +89,28 @@ def make_choice(label):
         return neg_mov
 
 
+def handle_eval_data(myDlg, uid="Info personal"):
+    ok_data = myDlg.show()
+    if myDlg.OK:  # or if ok_data is not None
+        print("[I] %s: %s" % (uid, ok_data))
+        if (uid != "Info personal"):
+            feedback.append(ok_data[0])
+            logging.log("Dato registrado %s: %s" % (uid, ok_data[0]), level=80)
+        else:  # Info del sujeto
+            logging.log("#### Nuevo experimento ####", level=80)
+            logging.log("Edad sujeto: " + str(ok_data[0]), level=80)
+            logging.log("Sexo sujeto: " + str(ok_data[1]), level=80)
+
+    else:
+        raise (RuntimeError, "El usuario se la mando")
+
+
+# obtener info del sujeto
 myDlg = gui.Dlg(title="Experimento emocionalidad")
 myDlg.addText('Informacion del sujeto')
 myDlg.addField('Edad:')
 myDlg.addField('Sexo:', choices=["Masculino", "Femenino"])
-ok_data = myDlg.show()  # show dialog and wait for OK or Cancel
-if myDlg.OK:  # or if ok_data is not None
-    print("Data usuario: %s" % ok_data)
-else:
-    raise (RuntimeError, "El usuario se la mando")
-
-# imprimir informacion al logfile
-logging.log("#### Nuevo experimento ####", level=80)
-logging.log("Edad sujeto: " + str(ok_data[0]), level=80)
-logging.log("Sexo sujeto: " + str(ok_data[1]), level=80)
+handle_eval_data(myDlg)
 
 for ii, trial in trials.iterrows():
     # intervalo entre videos
@@ -116,7 +127,8 @@ for ii, trial in trials.iterrows():
     mov = choice(make_choice(label))
     # tiempo en el que aparece el estimulo
     onset = int(mov.filename.split('.')[0].split('_')[1])
-    print("Onset en: %s" % onset)
+    logging.log("Archivo: %s" % mov.filename, level=80)
+    print("[I] Onset en: %s segundos" % onset)
     start = time()
     isAlreadySent = False
     while mov.status != visual.FINISHED:
@@ -127,10 +139,13 @@ for ii, trial in trials.iterrows():
             timestamp = local_clock()
             print("[I] Evento con etiqueta %s enviado en %s: " %
                   (label, (time() - start)))
+            print("Se envia %s de tipo %s" % ([markernames[label]],
+                                              type([markernames[label]])))
             outlet.push_sample([markernames[label]], timestamp)
             isAlreadySent = True
 
         if len(event.getKeys()) > 0:
+            mov.stop()
             break
 
         event.clearEvents()
@@ -145,11 +160,7 @@ for ii, trial in trials.iterrows():
     video anterior. Utilice la imagen como referencia.')
     myDlg.addField(
         'Puntuacion:', choices=['1', '2', '3', '4', '5', '6', '7', '8', '9'])
-    ok_data = myDlg.show()  # show dialog and wait for OK or Cancel
-    if myDlg.OK:  # or if ok_data is not None
-        print(ok_data)
-    else:
-        print('user cancelled')
+    handle_eval_data(myDlg, 'valence')
     sam_arousal.draw()
     mywin.flip()
     core.wait(2.0)
@@ -158,12 +169,13 @@ for ii, trial in trials.iterrows():
     video anterior. Utilice la imagen como referencia.')
     myDlg.addField(
         'Puntuacion:', choices=['1', '2', '3', '4', '5', '6', '7', '8', '9'])
-    ok_data = myDlg.show()  # show dialog and wait for OK or Cancel
-    if myDlg.OK:  # or if ok_data is not None
-        print(ok_data)
-    else:
-        print('user cancelled')
+    handle_eval_data(myDlg, 'arousal')
 
+    # envio la operacion del usuario
+    dato = [int(''.join(feedback[-2:]))]
+    print(dato)
+    timestamp = local_clock()
+    outlet.push_sample(dato, local_clock())
     event.clearEvents()
 
     if len(event.getKeys()) > 0 or (time() - start) > record_duration:
